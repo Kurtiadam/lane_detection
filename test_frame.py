d0734ae3f359796590ed771e48cfc396314b96c8
Kurtiadam <adamkurti97@gmail.com>
2022-11-18 15:02:38 +0100

    ADD: unittest comment

--- test_frame.py
+++ test_frame.py
@@ -7,6 +7,8 @@ class FrameDimensions(unittest.TestCase):
             self.frame = FrameDB('example_material\lane_video.mkv')
             self.streaming, self.base  = self.frame.import_frame()
             self.assertEqual(np.size(self.base),1080*1920*3)
+            "Unit test for the individual frames if they are 1920*1080*3 \
+            (3 channels)"
 
 if __name__ == '__main__':
     unittest.main()
