ed1ca08208d9e0ba7fe5543b97a7e2db5bf8a09a
Kurtiadam <adamkurti97@gmail.com>
2022-11-22 23:56:08 +0100

    ADD: calling downscaling and cropping in main

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 from frame import FrameDB
+from resmod import ResMod
+from visualizer import Visualizer
 import cv2 as cv
+import numpy as np
+
 
 class LaneDetection:
     """Class for running the lane detection algorithm"""
-    def __init__(self, frame:FrameDB('example_material\lane_video.mkv')):
+
+    def __init__(self, frame: FrameDB = FrameDB('example_material\lane_video.mkv'), res_mod: ResMod = ResMod(), visualizer: Visualizer = Visualizer()):
         """Initializing input classes
 
         Args:
             frame (FrameDB): Class for handling the example video file
+            res_mod (ResMod): Class for resolution modifying methods
+            visualizer (Visualizer): Class for visualizing the results
         """
         self.frame = frame
+        self.res_mod = res_mod
+        self.visualizer = visualizer
 
     def run(self) -> None:
         """Running the the stream of the video file"""
-        while(self.frame.streaming):
-            self.streaming, imported_frame = self.frame.import_frame()
+        while (self.frame.streaming):
+            self.streaming, imp_frame = self.frame.import_frame()
+            downscaled = self.res_mod.downscale(imp_frame)
+            ds_cr, disc = self.res_mod.extract_roi(downscaled)
+            self.visualizer.render(ds_cr)
             # Stopping streaming with q key
             if cv.waitKey(1) == ord('q'):
                 break
@@ -23,7 +35,8 @@ class LaneDetection:
 
 
 def main():
-    lane_detector = LaneDetection(frame = FrameDB('example_material\lane_video.mkv'))
+    lane_detector = LaneDetection(frame=FrameDB(
+        'example_material\lane_video.mkv'), res_mod=ResMod(), visualizer=Visualizer())
     lane_detector.run()
 
 
