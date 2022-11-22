ab5ef48101f5f5dceee860e0a7add58bc2ed0012
Kurtiadam <adamkurti97@gmail.com>
2022-11-22 23:50:51 +0100

    Type hint for visualizer input

--- visualizer.py
+++ visualizer.py
@@ -1,8 +1,9 @@
 import cv2 as cv
+import numpy as np
 
 class Visualizer():
     @staticmethod
-    def render(frame):
+    def render(frame: np.ndarray):
         """
         This function shows the image in a new window to the user.
         """
