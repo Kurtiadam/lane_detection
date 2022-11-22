60b25e5a9867225648a8c11bc739fec9ca10cc1e
Kurtiadam <adamkurti97@gmail.com>
2022-11-22 23:52:23 +0100

    ADD: constants for resmod and colorspacetransform

--- constants.py
+++ constants.py
@@ -1,9 +1,16 @@
 from enum import Enum
 import numpy as np
 
-class CropConst(Enum):
-    """Constants for the cropping function"""
-    LOWER_YELLOW = np.array([25,70,100])
-    UPPER_YELLOW = np.array([70,200,255])
-    LOWER_WHITE = np.array([27,122,25])
-    UPPER_WHITE = np.array([101,255,150])
+
+class ResModConst(Enum):
+    """Constants for the downscaling and cropping function"""
+    CROP_VERT_START = 200
+    CROP_HOR_START = 0
+
+
+class ColorSpaceTransConst(Enum):
+    """Constants for the HLS color space function"""
+    # LOWER_YELLOW = np.array([25,70,100])
+    # UPPER_YELLOW = np.array([70,200,255])
+    # LOWER_WHITE = np.array([27,122,25])
+    # UPPER_WHITE = np.array([101,255,150])
