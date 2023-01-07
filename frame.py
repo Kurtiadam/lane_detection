22147f2b2804c4857a9cae441c98d218ab8e2149
Kurtiadam <adamkurti97@gmail.com>
2023-01-07 18:33:44 +0100

    CHG: path name, output dir check

--- frame.py
+++ frame.py
@@ -1,19 +1,20 @@
 import cv2 as cv
 import numpy as np
 import os
+from os import path
 
 
 class FrameDB:
     """Class for interaction with the video file"""
 
-    def __init__(self, path: str = 'example_material\\example_video.mp4') -> None:
+    def __init__(self, import_path: str = 'example_material\\example_video.mp4') -> None:
         """Capturing the video
 
         Args:
-            path (str): Location of the example video
+            import_path (str): Location of the example video
         """
         self.streaming = True
-        self.cap = cv.VideoCapture(path)
+        self.cap = cv.VideoCapture(import_path)
         self.mod_frame_array = []
 
     def import_frame(self) -> tuple[bool, np.ndarray]:
@@ -36,7 +37,8 @@ class FrameDB:
     def save_to_database(self, frame_array):
         height, width, layers = frame_array[0].shape
         size = (width, height)
-        os.mkdir('output')
+        if not path.exists('output'):
+            os.mkdir('output')
         os.chdir('output')
         out = cv.VideoWriter('detected_lanes.avi',
                              cv.VideoWriter_fourcc(*'XVID'), 24.0, size)
